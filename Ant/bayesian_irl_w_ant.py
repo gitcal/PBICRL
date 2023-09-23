bac95b9345babcd1b3d3e36401db667c29788ab3
Dimitris Papadimitriou <42616501+gitcal@users.noreply.github.com>
2023-09-23 01:31:15 -0700

    Add files via upload

---
+++ Ant/bayesian_irl_w_ant.py
@@ -0,0 +1,197 @@
+import numpy as np
+from mdp_utils import logsumexp
+import copy
+from random import choice
+import IPython
+from scipy.stats import bernoulli
+
+class BIRL:
+
+    def __init__(self, demos, trajectory_demos_states, preferences, beta_1, beta_2, beta_3, beta_4, num_samples, s, m1, m2, m3, m4, norm_flag):
+
+        """
+        Class for running and storing output of mcmc for Bayesian IRL
+        env: the mdp (we ignore the reward)
+        demos: list of (s,a) tuples 
+        beta: the assumed boltzman rationality of the demonstrator
+        """
+        self.demonstrations = demos
+        self.preferences = preferences
+        self.beta1 = beta_1
+        self.beta2 = beta_2
+        self.beta3 = beta_3
+        self.beta4 = beta_4
+        self.norm_flag = norm_flag
+        self.num_samples = num_samples
+        self.s = s
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+        self.m4 = m4
+        self.traj_state_demos = trajectory_demos_states
+
+    def logsumexp(x):
+
+        max_x = np.max(x)
+        sum_exp = 0.0
+        for xi in x:
+            sum_exp += np.exp(xi - max_x)
+        return max(x) + np.log(sum_exp)
+
+
+    def calc_ll_pref(self, rew_tuple,  trajectories, preferences):
+
+        #perform hypothetical given current reward hypothesis
+        log_prior = 0.0  #assume unimformative prior
+        log_sum = log_prior
+        W_fix, cur_W_ind, cur_rew, cur_x_loc = rew_tuple
+        for k in range(len(preferences)):# iterate over 3 categories of pairwise preferences
+            for itr in range(self.num_samples):
+                ind1 = preferences[k][itr][0]
+                ind2 = preferences[k][itr][1]
+                feat_traj1 = np.squeeze(trajectories[ind1]).T
+                feat_traj2 = np.squeeze(trajectories[ind2]).T
+                rew1 = np.dot(feat_traj1[:,:2], W_fix)
+                rew2 = np.dot(feat_traj2[:,:2], W_fix)
+                rew1_temp = 1*(feat_traj1[:,2]>cur_x_loc) * cur_rew * cur_W_ind
+                rew2_temp = 1*(feat_traj2[:,2]>cur_x_loc) * cur_rew * cur_W_ind
+                rew1 = rew1 + rew1_temp
+                rew2 = rew2 + rew2_temp
+
+                if self.norm_flag:
+                    if k==0:
+                        log_sum += self.s*(self.beta1 * np.mean(rew1)) - logsumexp([self.s*(self.beta1 * np.mean(rew1)), self.s*self.beta1 * np.mean(rew2)]) 
+                    if k==1:
+                        log_sum += self.s*(self.beta2 * np.mean(rew1)) - logsumexp([self.s*(self.beta2 * np.mean(rew1)), self.s*self.beta2 * np.mean(rew2)]) 
+                    if k==2:
+                        log_sum += self.s*(self.beta3 * np.mean(rew1)) - logsumexp([self.s*(self.beta3 * np.mean(rew1)), self.s*self.beta3 * np.mean(rew2)])
+                    if k==3:
+                        log_sum += self.s*(self.beta4 * np.mean(rew1)) - logsumexp([self.s*(self.beta4 * np.mean(rew1)), self.s*self.beta4 * np.mean(rew2)])
+        
+                else:
+                    if k==0:
+                        log_sum += self.s*(self.beta1 * np.sum(rew1)) - logsumexp([self.s*(self.beta1 * np.sum(rew1)), self.s*self.beta1 * np.sum(rew2)]) 
+                    if k==1:
+                        log_sum += self.s*(self.beta2 * np.sum(rew1)) - logsumexp([self.s*(self.beta2 * np.sum(rew1)), self.s*self.beta2 * np.sum(rew2)]) 
+                    if k==2:
+                        log_sum += self.s*(self.beta3 * np.sum(rew1)) - logsumexp([self.s*(self.beta3 * np.sum(rew1)), self.s*self.beta3 * np.sum(rew2)])
+                    if k==3:
+                        log_sum += self.s*(self.beta4 * np.mean(rew1)) - logsumexp([self.s*(self.beta4 * np.mean(rew1)), self.s*self.beta4 * np.mean(rew2)])
+        
+        return log_sum
+    
+
+    
+    
+
+    def generate_proposal_bern_constr_alternating(self, W_ind_old, rew_old, x_loc_old, ind, stdev1=1, stdev2=1):
+
+        rew_new = copy.deepcopy(rew_old)
+        W_ind_new = copy.deepcopy(W_ind_old)
+        x_loc_new = copy.deepcopy(x_loc_old)
+
+        if ind % 3 == 0:  
+            rew_new = rew_old + stdev1 * np.random.randn() 
+        elif ind % 3 == 1:
+            W_ind_new = 1 if W_ind_old == 0 else 0
+        else:
+            x_loc_new = x_loc_old + stdev2 * np.random.randn() 
+            
+        return W_ind_new, rew_new, x_loc_new
+
+
+    def initial_solution_bern_cnstr(self):
+
+        # initialize problem solution for MCMC to all zeros, maybe not best initialization but it works in most cases
+        pen_rew = np.random.uniform(-100, -10)#, self.env.num_states)
+        x_loc_new = 5*np.random.randn()
+        W_ind_new = np.random.randint(2)
+        return W_ind_new, pen_rew, x_loc_new
+
+   
+
+  
+
+    def run_mcmc_bern_constraint(self, samples, W_fix, stdev1, stdev2):
+
+        '''
+            run metropolis hastings MCMC with Gaussian symmetric proposal and uniform prior
+            samples: how many reward functions to sample from posterior
+            stepsize: standard deviation for proposal distribution
+            normalize: if true then it will normalize the rewards (reward weights) to be unit l2 norm, otherwise the rewards will be unbounded
+        '''
+        
+        num_samples = samples  # number of MCMC samples
+
+        accept_cnt = 0  #keep track of how often MCMC accepts, ideally around 40% of the steps accept
+
+        self.chain_W_ind = np.zeros((num_samples, 1)) #store rewards found via BIRL here, preallocate for speed
+        self.chain_rew = np.zeros((num_samples, 1)) 
+        self.chain_x_loc = np.zeros((num_samples, 1)) 
+        cur_W_ind, cur_rew, cur_x_loc = self.initial_solution_bern_cnstr()
+        cur_sol = (W_fix, cur_W_ind, cur_rew, cur_x_loc)
+        
+        cur_ll = self.calc_ll_pref(cur_sol, self.demonstrations, self.preferences)  # log likelihood
+        #keep track of MAP loglikelihood and solution
+        map_ll = cur_ll  
+        map_W_ind = cur_W_ind
+        map_rew = cur_rew
+        map_x_loc = cur_x_loc
+        for i in range(num_samples):
+            prop_W_ind, prop_rew, prop_x_loc = self.generate_proposal_bern_constr_alternating(cur_W_ind, cur_rew, cur_x_loc, i, stdev1=1, stdev2=0.5)
+            prop_sol = (W_fix, prop_W_ind, prop_rew, prop_x_loc)
+            prop_ll = self.calc_ll_pref(prop_sol, self.demonstrations, self.preferences)
+          
+            if prop_ll > cur_ll:
+                self.chain_W_ind[i] = prop_W_ind
+                self.chain_rew[i] = prop_rew
+                self.chain_x_loc[i] = prop_x_loc
+                accept_cnt += 1
+                cur_W_ind = prop_W_ind
+                cur_rew = prop_rew
+                cur_x_loc = prop_x_loc
+                cur_ll = prop_ll
+                if prop_ll > map_ll:  # maxiumum aposterioi
+                    map_ll = prop_ll
+                    map_W_ind = cur_W_ind
+                    map_rew = cur_rew
+                    map_x_loc = cur_x_loc
+            else:
+                # accept with prob exp(prop_ll - cur_ll)
+                if np.random.rand() < np.exp(prop_ll - cur_ll):
+                    self.chain_W_ind[i] = prop_W_ind
+                    self.chain_rew[i] = prop_rew
+                    self.chain_x_loc[i] = prop_x_loc
+                    accept_cnt += 1
+                    cur_W_ind = prop_W_ind
+                    cur_rew = prop_rew
+                    cur_x_loc = prop_x_loc
+                    cur_ll = prop_ll
+                else:
+                    # reject
+                    self.chain_W_ind[i] = cur_W_ind
+                    self.chain_rew[i] = cur_rew
+                    self.chain_x_loc[i] = cur_x_loc
+        
+        self.accept_rate = accept_cnt / num_samples
+        self.map_rew = map_rew
+        self.map_W_ind = map_W_ind
+        self.map_x_loc = map_x_loc
+    
+
+    def get_map_solution(self):
+
+        return self.map_W_ind, self.map_rew, self.map_x_loc
+
+
+    def get_mean_solution(self, burn_frac=0.1, skip_rate=1):
+
+        ''' get mean solution after removeing burn_frac fraction of the initial samples and only return every skip_rate
+            sample. Skiping reduces the size of the posterior and can reduce autocorrelation. Burning the first X% samples is
+            often good since the starting solution for mcmc may not be good and it can take a while to reach a good mixing point
+        '''
+        Chain_W_ind = self.chain_W_ind#[burn_frac::skip_rate]
+        Chain_rew = self.chain_rew
+        Chain_x_loc = self.chain_x_loc
+        
+        return Chain_W_ind, Chain_rew, Chain_x_loc
---
+++ Ant/main_pbirl_w_ant.py
@@ -0,0 +1,161 @@
+import bayesian_irl_w_ant
+import copy
+import IPython
+import numpy as np
+import pickle, argparse
+import itertools
+from itertools import permutations
+from random import sample
+import random
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--boltz_beta1', type=float, default=1)
+parser.add_argument('--boltz_beta2', type=float, default=1)
+parser.add_argument('--boltz_beta3', type=float, default=1)
+parser.add_argument('--boltz_beta4', type=float, default=1)
+parser.add_argument('--stdev1', type=float, default=1)
+parser.add_argument('--stdev2', type=float, default=0.5)
+parser.add_argument('--num_steps', type=int, default=20000)
+parser.add_argument('--iterations', type=int, default=1)
+parser.add_argument('--N_demonstrations', type=int, default=30)
+parser.add_argument('--flname', type=str, default='Ant_final_cnstr_100_cnstr1_10')
+parser.add_argument('--m1', type=float, default=0)
+parser.add_argument('--m2', type=float, default=0)
+parser.add_argument('--m3', type=float, default=0)
+parser.add_argument('--m4', type=float, default=0)
+parser.add_argument('--s', type=float, default=1)
+parser.add_argument('--norm_flag', type=int, default=1)
+args = parser.parse_args()
+
+
+
+def get_feat_rect(traj):	
+
+	dt = 0.05
+	st_list = []
+	single_state = []
+	act_list = []
+	for i in range(len(traj)-1):
+		tr = traj[i]
+		tr_p = traj[i+1]
+		st_tr, act_tr, st_rew = tr
+		st_tr_p, act_tr_p, st_rew_p = tr_p
+		st_list.append(st_rew_p-st_rew)
+		single_state.append(st_tr_p)
+		act_list.append(act_tr)
+	s_ = np.expand_dims(np.array(st_list),axis=1)
+	a_ = np.array(act_list)
+
+	single_state = np.array(single_state)
+	state = s_
+	feat_1 = s_/dt
+	feat_2 = np.expand_dims(np.linalg.norm(a_,axis=1)**2, axis=1)
+	feat_3 = np.expand_dims(single_state[:,0], axis=1) 
+	feat = np.stack((feat_1, feat_2, feat_3))
+	return feat
+
+
+
+def main():
+
+	boltz_beta1 = args.boltz_beta1
+	boltz_beta2 = args.boltz_beta2
+	boltz_beta3 = args.boltz_beta3
+	boltz_beta4 = args.boltz_beta4
+
+	num_steps = args.num_steps
+	iterations = args.iterations
+	stdev1 = args.stdev1
+	stdev2 = args.stdev2
+	N_demonstrations = args.N_demonstrations
+	fl_name = args.flname
+	m1 = args.m1
+	m2 = args.m2
+	m3 = args.m3
+	m4 = args.m4
+
+	s = args.s
+	norm_flag = args.norm_flag
+	res_dict = {}
+
+	W_fix = np.array([1,-0.5])
+	filename = 'data/' + fl_name + '_' + str(num_steps) + '_' + str(N_demonstrations)+\
+	'_'+str(boltz_beta1)+'_'+str(boltz_beta2)+'_'+str(boltz_beta3)+'_'+str(s)+'_'+str(m1)+'_'+str(m2)+'_'+str(m3)+'_'+str(norm_flag)       
+
+	with open('data/' + fl_name + '.pickle', 'rb') as pickle_file:
+		data = pickle.load(pickle_file)
+
+	trajectory_demos_states = data['trajectories']
+	rews = data['rewards']
+
+	traj_feat = []
+	res_map_cnstr = []
+	res_map_rew = []
+	for k in range(len(trajectory_demos_states)):# iterate over 3 categories of pairwise preferences
+		feats_traj = get_feat_rect(trajectory_demos_states[k])
+		traj_feat.append(feats_traj)
+	preferences = data['traj_index']
+
+	good_ind = np.where(preferences == 1)[0]
+	bad_ind = np.where(preferences == 2)[0]
+	vbad_ind = np.where(preferences == 3)[0]
+	vvbad_ind = np.where(preferences == 4)[0]
+	good_bad_pairs = []
+	bad_vbad_pairs = []
+	good_vbad_pairs = []
+	vbad_vvbad_pairs = []
+
+	for i in good_ind:
+		for j in bad_ind:
+			good_bad_pairs.append((i,j))
+
+	for i in bad_ind:
+		for j in vbad_ind:
+			bad_vbad_pairs.append((i,j))
+
+	for i in good_ind:
+		for j in vbad_ind:
+			good_vbad_pairs.append((i,j))
+
+	for i in vbad_ind:
+		for j in vvbad_ind:
+			vbad_vvbad_pairs.append((i,j))
+	
+
+	for iters in range(iterations):
+		time_start = time.time()
+		print('Iteration {} for beta1 {} beta2 {} beta3 {} s {} m1 {} m2 {} m3 {} demos {} norm {}'.format(iters, boltz_beta1, \
+			boltz_beta2, boltz_beta3, s, m1, m2, m3, N_demonstrations, norm_flag))
+
+		random.shuffle(good_bad_pairs)
+		random.shuffle(bad_vbad_pairs)
+		random.shuffle(good_vbad_pairs)
+		random.shuffle(vbad_vvbad_pairs)
+		
+		pref_pairs = [good_bad_pairs]#, bad_vbad_pairs, good_vbad_pairs]#,vbad_vvbad_pairs]
+
+		d12 = []
+		d23 = []
+		d13 = []
+		for ii in range(1):
+			birl = bayesian_irl_w_ant.BIRL(traj_feat, trajectory_demos_states, pref_pairs, boltz_beta1, boltz_beta2, boltz_beta3,boltz_beta4, N_demonstrations, s, m1, m2, m3, m4, norm_flag)
+			birl.run_mcmc_bern_constraint(num_steps, W_fix, stdev1, stdev2)
+			acc_rate = birl.accept_rate
+			print('acceptance rate {}'.format(acc_rate)) #self.map_W_ind, self.map_rew, self.map_x_loc
+			chain_W_ind, chain_rew, chain_x_loc = birl.get_mean_solution(burn_frac=0.2, skip_rate=1)
+			map_W_ind, map_rew, map_x_loc = birl.get_map_solution()
+			res_dict[iters,'indicators'] = map_W_ind
+			res_dict[iters,'map_rew'] = map_rew
+			res_dict[iters,'map_x_loc'] =map_x_loc 
+			res_dict[iters,'chain_W_ind'] = chain_W_ind[-500:,:]
+			res_dict[iters,'chain_rew'] = chain_rew[-500:,:]
+			res_dict[iters,'chain_x_loc'] = chain_x_loc[-500:,:]
+			res_dict[iters,'time'] = time.time()-time_start
+
+		print('Time of iteration {} is {}'.format(iters, time.time()-time_start))
+
+	with open(filename + '.pickle', 'wb') as handle:
+		pickle.dump(res_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+if __name__=="__main__":
+	main()
